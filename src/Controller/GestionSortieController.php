f08bcec78f9ff8ee31c2986f2169a112c1744769
Dimitri <dig72000@gmail.com>
2021-08-10 11:52:06 +0200

    Creation annulation sortie formulaire + twig + controller

---
+++ src/Controller/GestionSortieController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class GestionSortieController extends AbstractController
+{
+    /**
+     * @Route("/gestion/sortie", name="gestion_sortie")
+     */
+    public function index(): Response
+    {
+        return $this->render('gestion_sortie/index.html.twig', [
+            'controller_name' => 'GestionSortieController',
+        ]);
+    }
+
+    /**
+     * @Route("/gestion/annulationsortie", name="gestion_sortie/annuler")
+     */
+    public function annulerSortie(Request $request, EntityManagerInterface $entityManager) : Response {
+
+        $sortieC = new Sortie();
+        $sortieCForm = $this->createForm($sortieC);
+        $sortieCForm->handleRequest($request);
+
+        if($sortieCForm->isSubmitted() && $sortieCForm->isValid()) {
+        //        $wishes->setDateCreated(new \DateTime());
+
+            $entityManager->persist($sortieC);
+            $entityManager->flush();
+
+            $this->addFlash('sucess', 'Annulation de votre sortie, validée');
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('gestion_sortie/sortieannulee.html.twig');
+
+    }
+
+}

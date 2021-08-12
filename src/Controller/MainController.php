f12777a66d1cb0c2262cca97697e3e1a6314bbcf
Maxime Braud <maxime.braud86@gmail.com>
2021-08-12 11:15:23 +0200

    Mise en place du tableau de la page d'acceuil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,6 +8,8 @@ use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class MainController extends AbstractController
 {
@@ -16,16 +18,36 @@ class MainController extends AbstractController
      */
     public function home(SortieRepository $sortieRepository): Response
     {
+        $user = $this->getUser();
         $FilterActivityForm = $this->createForm(FilterActivityType::class);
 
         //todo: Traiter le formulaire du filtre page d'acceuil
 
         $sorties = $sortieRepository->findSorties();
 
+        //verification inscription
+
+        $verifInsciption=array();
+
+        $i=0;
+        foreach($sorties as $sortie){
+          $tableauParticipants = $sortie->getParticipants() ;
+          $verifInsciption[$i] = false;
+          foreach ($tableauParticipants as $participant){
+              if($participant->getId() == $user->getId())
+              {
+                  $verifInsciption[$i] = true;
+
+              }
+
+          }
+          $i++;
+        }
 
         return $this->render('main/home.html.twig', [
             'FilterActivityForm' => $FilterActivityForm->createView(),
-            'sorties' => $sorties
+            'sorties' => $sorties,
+            'verifInsciption'=>$verifInsciption
             ]);
     }
 }

f2f722db175c7e753996aeeed21a520fe5dd42ab
RomainBEGUE <romain.begue@live.fr>
2021-08-19 11:11:49 +0200

    Mise à jour suppression des lieux et des villes

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -46,9 +46,11 @@ class Lieu
     private $ville;
 
     /**
-     * @ORM\OneToMany(targetEntity=Sortie::class, mappedBy="lieu")
+     * @ORM\OneToMany(targetEntity=Sortie::class, mappedBy="lieu", orphanRemoval=true)
      */
     private $sorties;
+
+
 
     public function __construct()
     {
@@ -149,4 +151,5 @@ class Lieu
 
         return $this;
     }
+
 }
